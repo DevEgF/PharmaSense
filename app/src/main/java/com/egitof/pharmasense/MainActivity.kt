d79f6d24f41c6932d559fdfc0d8c3e29cc2f4496
Egito Filho <egitofilho4@gmail.com>
2025-04-19 08:16:21 -0300

    first commit

---
+++ app/src/main/java/com/egitof/pharmasense/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.egitof.pharmasense
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.egitof.pharmasense.ui.theme.AppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    Test()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Test() {
+    val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.loading))
+
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        iterations = LottieConstants.IterateForever
+    )
+    LottieAnimation(
+        composition = composition,
+        progress = progress
+    )
+}
